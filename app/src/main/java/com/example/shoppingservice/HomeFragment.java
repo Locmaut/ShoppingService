f639e79796016d4caa081df203f9ba9909d295fe
Maksim <locmaut@gmail.com>
2023-09-23 01:53:22 +0300

    2.1 Update

---
+++ app/src/main/java/com/example/shoppingservice/HomeFragment.java
@@ -0,0 +1,84 @@
+package com.example.shoppingservice;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shoppingservice.adapters.AdsAdapter;
+import com.example.shoppingservice.adapters.CategoriesAdapter;
+import com.example.shoppingservice.data.AdsItem;
+import com.example.shoppingservice.data.CategoriesItem;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class HomeFragment extends Fragment {
+    RecyclerView categoriesRecyclerView;
+    CategoriesAdapter categoriesAdapter;
+    List<CategoriesItem> categoriesList;
+
+    RecyclerView adsRecyclerView;
+    AdsAdapter adsAdapter;
+    List<AdsItem> adsList;
+
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        categoriesList = generateCategoriesItems();
+        categoriesRecyclerView = rootView.findViewById(R.id.recyclerView_home_categories);
+        categoriesAdapter = new CategoriesAdapter(categoriesList);
+        categoriesRecyclerView.setAdapter(categoriesAdapter);
+
+        adsList = generateAdsItems();
+        adsRecyclerView = rootView.findViewById(R.id.recyclerView_home_premium_ads);
+        adsAdapter = new AdsAdapter(adsList);
+        adsRecyclerView.setAdapter(adsAdapter);
+        return rootView;
+    }
+
+    private List<CategoriesItem> generateCategoriesItems() {
+        List<CategoriesItem> categoriesList = List.of(
+                new CategoriesItem(R.drawable.btn_business_and_services, getString(R.string.business_and_services)),
+                new CategoriesItem(R.drawable.btn_free, getString(R.string.free)),
+                new CategoriesItem(R.drawable.btn_children_world, getString(R.string.children_world)),
+                new CategoriesItem(R.drawable.btn_home_and_garden, getString(R.string.home_and_garden)),
+                new CategoriesItem(R.drawable.btn_help, getString(R.string.help)),
+                new CategoriesItem(R.drawable.btn_electronic, getString(R.string.electronic)),
+                new CategoriesItem(R.drawable.btn_beauty_and_health, getString(R.string.beauty_and_health)),
+                new CategoriesItem(R.drawable.btn_fashion_and_style, getString(R.string.fashion_and_style)),
+                new CategoriesItem(R.drawable.btn_real_estate, getString(R.string.real_estate)),
+                new CategoriesItem(R.drawable.btn_work, getString(R.string.work)),
+                new CategoriesItem(R.drawable.btn_animal, getString(R.string.animal)),
+                new CategoriesItem(R.drawable.btn_transport, getString(R.string.transport)),
+                new CategoriesItem(R.drawable.btn_hobby, getString(R.string.hobby))
+        );
+        return categoriesList;
+    }
+
+    private List<AdsItem> generateAdsItems() {
+        List<AdsItem> adsItems = List.of(
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "TitleTitleTitleTitleTitleTitleTitleTitleTitleTitle", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired),
+                new AdsItem(R.drawable.rectangle, "Title", "Poltava", LocalDateTime.now(), 2500, R.drawable.ads_premium, R.drawable.ads_desired)
+        );
+        return adsItems;
+    }
+}
